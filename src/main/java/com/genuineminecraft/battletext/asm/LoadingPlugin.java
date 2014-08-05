5463d3d746b408c39bd3d0e2b0cd13e20a1ffcf9
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2014-08-05 01:01:53 -0700

    ASM Version Update

--- src/main/java/com/genuineminecraft/battletext/asm/LoadingPlugin.java
+++ src/main/java/com/genuineminecraft/battletext/asm/LoadingPlugin.java
@@ -48,7 +48,7 @@ public class LoadingPlugin implements IFMLLoadingPlugin {
 			md.authorList = Arrays.asList("Genuine");
 			md.modId = "<BattleText ASM>";
 			md.name = md.description = "BattleText HealingCore";
-			md.version = "000";
+			md.version = "1";
 		}
 
 		@Override
