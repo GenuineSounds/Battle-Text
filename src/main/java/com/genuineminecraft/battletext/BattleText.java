a9d578505b2b02076a2d326baa2282391c1a0c02
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2014-09-13 19:35:44 -0700

    Version Sync

--- src/main/java/com/genuineminecraft/battletext/BattleText.java
+++ src/main/java/com/genuineminecraft/battletext/BattleText.java
@@ -32,7 +32,7 @@ public class BattleText {
 	public static BattleText instance;
 	public static final String MODID = "BattleText";
 	public static final String NAME = "[Genuine] Battle Text";
-	public static final String VERSION = "1.6";
+	public static final String VERSION = "1.7.10-r1";
 	public static File dir;
 	public static Configuration config;
 
