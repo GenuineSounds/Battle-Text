f136c4e1f10cf1493c5ec8b9ab70a86e2fa5b5e2
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2015-01-23 19:04:33 -0800

    Version and Image Fixes

--- src/main/java/com/genuineminecraft/battletext/BattleText.java
+++ src/main/java/com/genuineminecraft/battletext/BattleText.java
@@ -30,7 +30,7 @@ public class BattleText {
 	public static BattleText instance;
 	public static final String MODID = "BattleText";
 	public static final String NAME = "Battle Text";
-	public static final String VERSION = "1.7.10-r3";
+	public static final String VERSION = "1.7.10-1.0.5";
 	public static File dir;
 	public static Configuration config;
 
