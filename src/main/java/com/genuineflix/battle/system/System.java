d274e9d34287a03bd31ed396ef627ae71b440574
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2015-01-29 17:51:54 -0800

    Fixed synch issues.

--- src/main/java/com/genuineflix/battle/system/System.java
+++ src/main/java/com/genuineflix/battle/system/System.java
@@ -7,9 +7,10 @@ import java.util.List;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntityDamageSource;
 import net.minecraft.util.EntityDamageSourceIndirect;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
@@ -36,101 +37,125 @@ public class System {
 	public static final String CC_MOD_NAME = "ClosedCaption";
 	public static final String CC_DIRECT_MESSAGE_KEY = "[Direct]";
 	public static final System instance = new System();
-	public List<Text> textList = Collections.synchronizedList(new ArrayList<Text>());
+	private List<Text> textList = Collections.synchronizedList(new ArrayList<Text>());
 
 	public System() {}
 
 	@SubscribeEvent
-	public synchronized void entityHeal(final LivingHealEvent event) {
-		if (System.ccIsLoaded() && event.entityLiving.equals(Minecraft.getMinecraft().thePlayer)) {
-			final String amount = Integer.toString((int) event.amount);
-			final StringBuilder message = new StringBuilder();
-			message.append("Healing: ");
-			message.append(ChatFormatting.GREEN.toString());
-			message.append(amount);
-			message.append(ChatFormatting.RESET.toString());
-			FMLInterModComms.sendMessage(System.CC_MOD_NAME, System.CC_DIRECT_MESSAGE_KEY, message.toString());
+	public void entityHeal(final LivingHealEvent event) {
+		if (event.amount <= 0)
 			return;
-		} else if (event.amount >= 0)
-			textList.add(new Text(event.entityLiving, event.amount));
-	}
-
-	@SubscribeEvent
-	public synchronized void entityHurt(final LivingHurtEvent event) {
 		if (System.ccIsLoaded() && event.entityLiving.equals(Minecraft.getMinecraft().thePlayer)) {
-			String name = "";
-			if (event.source instanceof EntityDamageSource) {
-				final EntityDamageSource nds = (EntityDamageSource) event.source;
-				Entity src = null;
-				if (nds instanceof EntityDamageSourceIndirect)
-					src = ((EntityDamageSourceIndirect) nds).getEntity();
-				else
-					src = nds.getSourceOfDamage();
-				if (src instanceof EntityPlayer)
-					name = ((EntityPlayer) src).getDisplayName();
-				else
-					name = src.getCommandSenderName();
-			}
-			final String[] tmps = event.source.getDamageType().split("\\.");
-			String out = "";
-			for (final String string : tmps)
-				out += string.substring(0, 1).toUpperCase() + string.substring(1);
-			out = I18n.format(out.replaceAll("[A-Z]", " $0").trim());
-			if (name.isEmpty())
-				name = out;
-			else
-				name = ChatFormatting.BLUE + name + ChatFormatting.RESET + " -> " + out;
-			final StringBuilder message = new StringBuilder();
-			message.append(name);
-			message.append(": ");
-			message.append(ChatFormatting.DARK_RED);
-			message.append((int) event.ammount);
-			message.append(ChatFormatting.RESET);
-			FMLInterModComms.sendMessage(System.CC_MOD_NAME, System.CC_DIRECT_MESSAGE_KEY, message.toString());
+			NBTTagCompound tag = new NBTTagCompound();
+			tag.setString("type", "healing");
+			tag.setFloat("amount", event.amount);
+			FMLInterModComms.sendMessage(System.CC_MOD_NAME, System.CC_DIRECT_MESSAGE_KEY, tag);
 			return;
 		}
-		if (event.ammount >= 0)
-			textList.add(new Text(event.entityLiving, event.source, event.ammount));
+		synchronized (textList) {
+			textList.add(new Text(event.entityLiving, event.amount));
+		}
 	}
 
 	@SubscribeEvent
-	public synchronized void render(final RenderWorldLastEvent event) {
+	public void entityHurt(final LivingHurtEvent event) {
+		if (event.ammount <= 0)
+			return;
+		if (System.ccIsLoaded() && event.entityLiving.equals(Minecraft.getMinecraft().thePlayer)) {
+			NBTTagCompound tag = new NBTTagCompound();
+			tag.setString("type", "damage");
+			tag.setFloat("amount", event.ammount);
+			tag.setString("message", constructDamageMessage(event.source));
+			FMLInterModComms.sendMessage(System.CC_MOD_NAME, System.CC_DIRECT_MESSAGE_KEY, tag);
+			return;
+		}
+		synchronized (textList) {
+			textList.add(new Text(event.entityLiving, event.source, event.ammount));
+		}
+	}
+
+	private String constructDamageMessage(DamageSource source) {
+		StringBuilder message = beginDamageMessage(source);
+		message.append(constructDamageTypes(source));
+		message.append(": ");
+		message.append(ChatFormatting.DARK_RED);
+		return message.toString();
+	}
+
+	private String constructDamageTypes(DamageSource source) {
+		final String[] tmps = source.getDamageType().split("\\.");
+		StringBuilder sourceTypes = new StringBuilder();
+		for (final String string : tmps)
+			sourceTypes.append(string.substring(0, 1).toUpperCase() + string.substring(1));
+		return sourceTypes.toString().replaceAll("[A-Z]", " $0").trim();
+	}
+
+	private StringBuilder beginDamageMessage(DamageSource source) {
+		StringBuilder message = new StringBuilder();
+		if (!(source instanceof EntityDamageSource))
+			return message;
+		final EntityDamageSource nds = (EntityDamageSource) source;
+		Entity src = null;
+		if (nds instanceof EntityDamageSourceIndirect)
+			src = ((EntityDamageSourceIndirect) nds).getEntity();
+		else
+			src = nds.getSourceOfDamage();
+		if (src instanceof EntityPlayer)
+			message.append(damageSourceName(((EntityPlayer) src).getDisplayName()));
+		else
+			message.append(damageSourceName(src.getCommandSenderName()));
+		return message;
+	}
+
+	private String damageSourceName(String name) {
+		StringBuilder nameBuilder = new StringBuilder();
+		nameBuilder.append(ChatFormatting.BLUE);
+		nameBuilder.append(name);
+		nameBuilder.append(ChatFormatting.RESET);
+		nameBuilder.append(" -> ");
+		return nameBuilder.toString();
+	}
+
+	@SubscribeEvent
+	public void render(final RenderWorldLastEvent event) {
 		final FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
 		GL11.glPushMatrix();
 		GL11.glDisable(GL11.GL_LIGHTING);
 		GL11.glDisable(GL11.GL_DEPTH_TEST);
 		GL11.glEnable(GL11.GL_BLEND);
 		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		for (final Text txt : textList) {
-			if (txt.getDistanceTo(Minecraft.getMinecraft().thePlayer) > 32)
-				continue;
-			final double x = RenderManager.renderPosX - (txt.prevPosX + (txt.posX - txt.prevPosX) * event.partialTicks);
-			final double y = RenderManager.renderPosY - (txt.prevPosY + (txt.posY - txt.prevPosY) * event.partialTicks) - 2;
-			final double z = RenderManager.renderPosZ - (txt.prevPosZ + (txt.posZ - txt.prevPosZ) * event.partialTicks);
-			GL11.glTranslated(-x, -y, -z);
-			GL11.glRotatef(RenderManager.instance.playerViewY + 180, 0F, -1F, 0F);
-			GL11.glRotatef(RenderManager.instance.playerViewX, -1F, 0F, 0F);
-			int alpha = (int) (txt.getPercent() * 0xFF) & 0xFF;
-			if (alpha < 5)
-				alpha = 5;
-			final int color1 = txt.textColor | alpha << 24;
-			final int color2 = txt.backgroundColor | alpha << 24;
-			final int offX = -fr.getStringWidth(txt.display);
-			final int offY = -4;
-			double scale = 0.0175;
-			scale *= txt.getScale();
-			GL11.glScaled(scale, -scale, scale);
-			// Shadows
-			fr.drawString(txt.display, offX + 1, offY, color2);
-			fr.drawString(txt.display, offX, offY + 1, color2);
-			fr.drawString(txt.display, offX, offY - 1, color2);
-			fr.drawString(txt.display, offX - 1, offY, color2);
-			// Main
-			fr.drawString(txt.display, offX, offY, color1);
-			GL11.glScaled(1.0 / scale, -1.0 / scale, 1.0 / scale);
-			GL11.glRotatef(RenderManager.instance.playerViewX, 1F, 0F, 0F);
-			GL11.glRotatef(RenderManager.instance.playerViewY - 180, 0F, 1F, 0F);
-			GL11.glTranslated(x, y, z);
+		synchronized (textList) {
+			for (final Text txt : textList) {
+				if (txt.getDistanceTo(Minecraft.getMinecraft().thePlayer) > 32)
+					continue;
+				final double x = RenderManager.renderPosX - (txt.prevPosX + (txt.posX - txt.prevPosX) * event.partialTicks);
+				final double y = RenderManager.renderPosY - (txt.prevPosY + (txt.posY - txt.prevPosY) * event.partialTicks) - 2;
+				final double z = RenderManager.renderPosZ - (txt.prevPosZ + (txt.posZ - txt.prevPosZ) * event.partialTicks);
+				GL11.glTranslated(-x, -y, -z);
+				GL11.glRotatef(RenderManager.instance.playerViewY + 180, 0F, -1F, 0F);
+				GL11.glRotatef(RenderManager.instance.playerViewX, -1F, 0F, 0F);
+				int alpha = (int) (txt.getPercent() * 0xFF) & 0xFF;
+				if (alpha < 5)
+					alpha = 5;
+				final int color1 = txt.textColor | alpha << 24;
+				final int color2 = txt.backgroundColor | alpha << 24;
+				final int offX = -fr.getStringWidth(txt.display);
+				final int offY = -4;
+				double scale = 0.0175;
+				scale *= txt.getScale();
+				GL11.glScaled(scale, -scale, scale);
+				// Shadows
+				fr.drawString(txt.display, offX + 1, offY, color2);
+				fr.drawString(txt.display, offX, offY + 1, color2);
+				fr.drawString(txt.display, offX, offY - 1, color2);
+				fr.drawString(txt.display, offX - 1, offY, color2);
+				// Main
+				fr.drawString(txt.display, offX, offY, color1);
+				GL11.glScaled(1.0 / scale, -1.0 / scale, 1.0 / scale);
+				GL11.glRotatef(RenderManager.instance.playerViewX, 1F, 0F, 0F);
+				GL11.glRotatef(RenderManager.instance.playerViewY - 180, 0F, 1F, 0F);
+				GL11.glTranslated(x, y, z);
+			}
 		}
 		GL11.glDisable(GL11.GL_BLEND);
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
@@ -139,18 +164,20 @@ public class System {
 	}
 
 	@SubscribeEvent
-	public synchronized void tick(final ClientTickEvent event) {
+	public void tick(final ClientTickEvent event) {
 		if (event.phase == Phase.START)
 			return;
 		final Minecraft mc = Minecraft.getMinecraft();
 		if (mc.thePlayer == null || mc.currentScreen != null && mc.currentScreen.doesGuiPauseGame())
 			return;
 		final List<Text> removalQueue = new ArrayList<Text>();
-		for (final Text text : textList)
-			if (!text.onUpdate())
-				removalQueue.add(text);
-		textList.removeAll(removalQueue);
+		synchronized (textList) {
+			for (final Text text : textList)
+				if (!text.onUpdate())
+					removalQueue.add(text);
+			textList.removeAll(removalQueue);
+			Collections.sort(textList);
+		}
 		removalQueue.clear();
-		Collections.sort(textList);
 	}
 }
