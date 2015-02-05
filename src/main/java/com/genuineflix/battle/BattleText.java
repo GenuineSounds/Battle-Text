fa9c4fa47ae735596c0a1bf9d33fe71da436cad0
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2015-02-04 21:28:04 -0800

    Version Change.

--- src/main/java/com/genuineflix/battle/BattleText.java
+++ src/main/java/com/genuineflix/battle/BattleText.java
@@ -31,7 +31,7 @@ public class BattleText {
 	public static BattleText instance;
 	public static final String MODID = "BattleText";
 	public static final String NAME = "BattleText";
-	public static final String VERSION = "1.0.11";
+	public static final String VERSION = "1.0.12";
 	public static File dir;
 	public static Configuration config;
 
