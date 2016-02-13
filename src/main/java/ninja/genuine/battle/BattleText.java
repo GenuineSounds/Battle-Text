831ce6997ec4df369b2a58117577e94ae7a498cc
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2016-02-12 22:23:48 -0800

    Build Upgrade

--- src/main/java/ninja/genuine/battle/BattleText.java
+++ src/main/java/ninja/genuine/battle/BattleText.java
@@ -29,7 +29,7 @@ public class BattleText {
 	public static BattleText instance;
 	public static final String MODID = "BattleText";
 	public static final String NAME = "BattleText";
-	public static final String VERSION = "1.0.16";
+	public static final String VERSION = "1.0.17";
 	public static File dir;
 	public static Configuration config;
 
